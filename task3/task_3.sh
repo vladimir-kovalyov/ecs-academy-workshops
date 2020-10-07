6dd89094005c8d67a8705e2bf6e283b8f55acd0d
Vladimirs Kovalovs <vladimirs.kovalovs@ecs-digital.co.uk>
2020-10-07 13:36:42 +0100

    Task 3 Completed

--- task3/task_3.sh
+++ task3/task_3.sh
@@ -1,11 +1,21 @@
-// 1
-value=0
-if [[ $value > 1 ]]; then
-      echo "Value is bigger."
+# task 3.1
+NUM1=5
+NUM2=7
+NUM3=6
+if [ $NUM2 -lt $NUM3 ]; then
+    echo "Number 2 is less then Number 3"
     exit 0
-else
-      echo "Value is smaller"
-    exit 1
+  elif [ $NUM1 -lt $NUM2 ]; then
+    echo "$NUM2 is bigger then $NUM1"
+    exit 100
+  else
+    echo "Number 2 is bigger then Number 3"
+    exit 50
 fi
-// 2
 
+
+# task 3.2
+# crontab -e
+# 00 12 * * * /Users/vladimir/Desktop/ecs-acam-scripting-workshop/task3/task_3.sh
+# :wq
+# crontab -l
